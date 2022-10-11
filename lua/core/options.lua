9c0443eb7b48f205c0b2963bbe479b3618f41d1a
xxCheatCodes <lv2497712968@outlook.com>
2022-10-11 12:35:45 +0800

    添加win32yank.exe作为wsl下的剪贴板

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -7,7 +7,7 @@ vim.opt.relativenumber = true -- 相对行号
 vim.opt.cursorline = true -- 高亮所在行
 vim.opt.signcolumn = "yes" -- 左侧图标指示列
 vim.opt.colorcolumn = "80" -- 换行参考线
-vim.opt.tabstop = 2
+vim.opt.tabstop = 2 -- tab缩进两个空格
 vim.opt.softtabstop = 2
 vim.opt.shiftround = true
 vim.opt.shiftwidth = 2
@@ -16,7 +16,7 @@ vim.opt.smartindent = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.incsearch = true
-vim.opt.cmdheight = 2
+vim.opt.cmdheight = 1 -- 编辑器命令行高度
 vim.opt.autoread = true
 vim.opt.wrap = false -- 折行
 vim.opt.hidden = true
@@ -25,7 +25,7 @@ vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.swapfile = false
 vim.opt.updatetime = 300
-vim.opt.timeoutlen = 500 -- 等待键盘快捷键连击时间
+vim.opt.timeoutlen = 300 -- 等待键盘快捷键连击时间(ms)
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.completeopt = "menu,menuone,noselect,noinsert" -- 自动补全不自动选中
@@ -39,3 +39,16 @@ vim.opt.showtabline = 2
 vim.opt.showmode = false -- vim 自带的模式提示
 vim.opt.clipboard = "unnamedplus" -- 系统剪贴板
 vim.opt.undofile = true -- 无限撤消
+-- vim.cmd("set guicursor=a:blinkwait700-blinkon400-blinkoff250") -- 光标闪烁
+
+-- Clipboard in WSL
+if vim.fn.has("wsl") and (not vim.g.vscode) then
+	vim.g.clipboard = {
+		name = 'wsl-clipboard',
+		copy = {["+"] = "clip.exe", ["*"] = "clip.exe"},
+		paste = {["+"] = "win32yank.exe -o --lf", ["*"] = "win32yank.exe -o --lf"},
+		cache_enabled = false
+	}
+end
+
+
