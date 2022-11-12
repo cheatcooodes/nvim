8bc8631c4ab09fb587338749ddab745f893c01b3
cheatcooodes <lv2497712968@outlook.com>
2022-11-13 01:30:33 +0800

    优化Packer配置，添加dap相关插件和comment插件

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -28,69 +28,105 @@ if not status_ok then
     return
 end
 
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+packer.init({
+		-- Maximum concurrency
+		max_jobs = 16,
+		-- Custom source
+		git = {
+			clone_timeout = 300, -- in seconds
+			-- default_url_format = ""
+		},
+		-- Floating window
+		display = {
+			open_fn = function()
+				return require("packer.util").float({ border = "single" })
+			end
+		}
+})
+
 -- Startup
 packer.startup(
 	{
 		function(use)
 
-			-- === PLUGINS ===
+			-- PLUGINS --
 
 			-- Packer
 			use {'wbthomason/packer.nvim'}
+
 			-- Icons
 			use({'kyazdani42/nvim-web-devicons'})
-			-- Status line
-			use({'nvim-lualine/lualine.nvim'})
-			-- use({'vim-airline/vim-airline'})
+
 			-- Utils
 			use {'windwp/nvim-autopairs'}
+			use {'numToStr/Comment.nvim'}
 			use {'tpope/vim-surround'}
 			use {'phaazon/hop.nvim', branch = 'v2'} -- an easymotion-like plugin
-			-- use {'unblevable/quick-scope'}
 			-- use {'folke/which-key.nvim'}
-			-- Lsp installer
-			use {'williamboman/mason.nvim'}
+
+			-- LSP
+			use {'neovim/nvim-lspconfig'} -- enable lsp
+			use {'williamboman/mason.nvim'} -- lsp installer
 			use {'williamboman/mason-lspconfig.nvim'}
-			-- Lspconfig
-			use {'neovim/nvim-lspconfig'}
-			-- AutoCompletion
+
+			-- Completion
 			use {'hrsh7th/nvim-cmp'}
 			use {'hrsh7th/cmp-nvim-lsp'}
 			use {'hrsh7th/cmp-buffer'}
 			use {'hrsh7th/cmp-path'}
 			use {'hrsh7th/cmp-cmdline'}
+
 			-- Snippets
-			use {'L3MON4D3/LuaSnip'}
-			use {'saadparwaiz1/cmp_luasnip'}
+			use {'L3MON4D3/LuaSnip'} -- snippets engine
+			use {'saadparwaiz1/cmp_luasnip'} -- snippets completions
 			use {'onsails/lspkind.nvim'} -- vscode-like icons for nvim-cmp menu 
 			use {'rafamadriz/friendly-snippets'} -- vscode-like snippets
+
 			-- Markdown support
 			use({'preservim/vim-markdown'})
 			use({'iamcco/markdown-preview.nvim',
 				run = function() vim.fn['mkdp#util#install']() end,
 			})
-			-- use({'joker1007/vim-markdown-quote-syntax'}) -- code syntax highlighting
+
 			-- Latex support
 			use({'lervag/vimtex'})
+
 			-- Treesitter
-			use {
-				'nvim-treesitter/nvim-treesitter',
+			use { 'nvim-treesitter/nvim-treesitter',
 				run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
 			}
+
 			-- Firenvim
 			use {'glacambre/firenvim', -- embedded neovim in browser
 				run = function() vim.fn['firenvim#install'](0) end
 			}
+
 			-- Debugger
 			use {'mfussenegger/nvim-dap'}
+			use {'rcarriga/nvim-dap-ui'}
+			use {'ravenxrz/DAPInstall.nvim'}
+
 			-- Visual marks
 			use {'kshenoy/vim-signature'}
+
 			-- Dashboard
 			-- use {''}
+
+			-- Status line
+			use({'nvim-lualine/lualine.nvim'}) -- or 'vim-airline/vim-airline'
+
 			-- Colorscheme
 			use {'joshdick/onedark.vim'}
 			use {'cormacrelf/vim-colors-github'}
-			use {'rafi/awesome-vim-colorschemes'} -- A set of colorschemes
+			use {'rafi/awesome-vim-colorschemes'} -- a bunch of colorschemes
 
 
 			if packer_bootstrap then
@@ -98,20 +134,5 @@ packer.startup(
 			end
 		end,
 
-		-- Packer configs
-		config = {
-			-- Maximum concurrency
-			max_jobs = 16,
-			-- Custom source
-			git = {
-				-- default_url_format = ""
-			},
-			-- Floating window
-			display = {
-				open_fn = function()
-					return require("packer.util").float({ border = "single" })
-				end
-			}
-		}
 	}
 )
